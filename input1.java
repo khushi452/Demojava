fd4394fdd62dc264ad868ffec54f69ff9e2dd6c5
khushi452 <khushisharmaclass10@gmail.com>
2026-01-30 12:43:13 +0530

    finding the area of the circle

--- input1.java
+++ input1.java
@@ -6,8 +6,10 @@ public class input1 {
         int n = S.nextInt();
         System.out.println(n);
         int m = S.nextInt();
+        System.out.println(m);
         System.out.println("the sum of n and m is:");
         System.out.println(n+m);
-
+        System.out.println("Area of the circle of radius n is :" + 3.14*n*n);
+        System.out.println("Area of the circle of radius m is :" + 3.14*m*m);
     }
 }
