3fa6045d4f677383e71ba9260c77143332e4a556
khushi452 <khushisharmaclass10@gmail.com>
2026-01-30 12:57:52 +0530

    reverse

---
+++ reversinguser.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class reversinguser {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.print("Enter a number to reverse: ");
+        int num = S.nextInt();
+        int reversed = 0
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println("Reversed number: " + reversed);
+        S.close();
+    }
+
+}
