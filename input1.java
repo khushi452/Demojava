3c06195d94edf6a3b43865d5db31324b2d87e2c5
khushi452 <khushisharmaclass10@gmail.com>
2026-01-30 12:39:16 +0530

    learning java

---
+++ input1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;  //implicit and explicit importing ways
+public class input1 {
+    public static void main (String[] args){
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = S.nextInt();
+        System.out.println(n);
+
+    }
+}
