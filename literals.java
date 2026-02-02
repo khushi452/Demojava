2e59095adc408ca284cc36a1914b9cc61f7492e6
khushi452 <khushisharmaclass10@gmail.com>
2026-02-02 11:58:50 +0530

    literals

---
+++ literals.java
@@ -0,0 +1,14 @@
+public class literals {
+     public static void main(String[] args){
+        int a = 12;
+        int b = 146;
+        int c = 0x73b;
+        int d = 0b1101;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+     }
+    
+}
