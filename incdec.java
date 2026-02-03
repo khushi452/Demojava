9334668aa1b6ef7c5d2c5384509dab351d7d1af1
khushi452 <khushisharmaclass10@gmail.com>
2026-02-03 10:16:15 +0530

    incrementdecrement

---
+++ incdec.java
@@ -0,0 +1,8 @@
+public class incdec {
+    
+    public static void main(String[] args) {
+        int a = 3;
+        int b = a++ + a++;
+        System.out.println("Initial value of b: " + b + " " + "a:" +a);
+    }
+}
