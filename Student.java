ccb31cb5b9e3e75aaeda10d1af6cf0b7bbedf531
khushi452 <khushisharmaclass10@gmail.com>
2026-02-05 14:28:46 +0530

    classes and method

---
+++ Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    int r;
+    String name;
+    void getdata(){
+        r=0;
+        name="ABC";
+    }
+    void display(){
+        System.out.println("Roll no: "+r);
+        System.out.println("Name: "+name);
+    }
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.getdata();
+        s1.display();
+        
+    }
+}
