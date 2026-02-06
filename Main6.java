8d9ca3d87984c92f7735862a3239b32259b6285a
khushi452 <khushisharmaclass10@gmail.com>
2026-02-06 12:39:11 +0530

    static and normal method

--- Main6.java
+++ Main6.java
@@ -1,17 +1,22 @@
 class Demo{
-    int r;
-    String name;
+    private int r;
+    private String name;
+    void getdata(int r, String name){
+       this.r=r;
+       this.name=name;
+    }
 
 }
-
+void show(){
+    System.out.println("Roll no: "+r);
+    System.out.println("Name: "+name);
+}
 
 
 public class Main6 {
     public static void main(String[] args) {
         Demo D = new Demo();
-        D.r=10;
-        D.name="Khushi";
-        System.out.println(D.r);
-        System.out.println(D.name);
+       D.getdata(10, "Khushi");
+       D.show();
     }
 }
