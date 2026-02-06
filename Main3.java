d42773db3d802e1d9cadd01b000dd7921f87bca3
khushi452 <khushisharmaclass10@gmail.com>
2026-02-06 09:57:15 +0530

    static variable

---
+++ Main3.java
@@ -0,0 +1,10 @@
+class StaticMethod{
+    static void print(){
+        System.out.println(" am static method");
+    }
+}
+public class Main3 {
+    public static void main(String[] args) {
+        StaticMethod.print();
+    }
+}
