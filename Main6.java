49995c9f27045ca7d16c778e1063da61a2fcba39
khushi452 <khushisharmaclass10@gmail.com>
2026-02-06 12:34:21 +0530

    static and normal method

---
+++ Main6.java
@@ -0,0 +1,17 @@
+class Demo{
+    int r;
+    String name;
+
+}
+
+
+
+public class Main6 {
+    public static void main(String[] args) {
+        Demo D = new Demo();
+        D.r=10;
+        D.name="Khushi";
+        System.out.println(D.r);
+        System.out.println(D.name);
+    }
+}
