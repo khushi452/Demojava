eea9fa6a042382d4dacae7163caa2d8b86cb5eae
khushi452 <khushisharmaclass10@gmail.com>
2026-02-06 09:59:58 +0530

    static Method

--- Main3.java
+++ Main3.java
@@ -1,10 +1,16 @@
 class StaticMethod{
-    static void print(){
-        System.out.println(" am static method");
+    static int add(int x,int y){
+        return x+y;
+    }
+    static int mul(int x,int y){
+        return x*y;
     }
 }
 public class Main3 {
     public static void main(String[] args) {
-        StaticMethod.print();
+        int result1=StaticMethod.add(5, 3);
+        int result2=StaticMethod.mul(5, 3);
+        System.out.println(result1);
+        System.out.println(result2);
     }
 }
