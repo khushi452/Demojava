9d77e7352689d8dd82e4756017edad1f1d39afb9
khushi452 <khushisharmaclass10@gmail.com>
2026-02-06 13:00:19 +0530

    default constructor

---
+++ Main7.java
@@ -0,0 +1,11 @@
+class Default{
+    Default(){
+        System.out.println("This is default constructor");
+    }
+}
+
+public class Main7 {
+    public static void main(String[] args) {
+        Default d = new Default();
+    }
+}
