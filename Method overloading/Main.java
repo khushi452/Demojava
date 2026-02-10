4136d4681ed82885c7f59c9077952b7c62ae8b4c
khushi452 <khushisharmaclass10@gmail.com>
2026-02-10 14:18:43 +0530

    method overloading

--- Method overloading/Main.java
+++ Method overloading/Main.java
@@ -1,12 +1,16 @@
 class Overload{
-    int add(int a, int b){
-        return a+b;
-    }
+    // // int add(int a, int b){
+    // //     return a+b;
+    // }
     int add(int a, int b, int c){
         return a+b+c;
+    // }
+    // long add(int a, double b){
+    //     return a+b;
     }
-    long add(int a, double b){
-        return a+b;
+    void add (long a,long b){
+        System.out.println(a);
+        System.out.println(b);
     }
 }
 public class Main {
@@ -16,8 +20,9 @@ public class Main {
         // System.out.println(r);
         // double d = o1.add(2,3.5);
         // System.out.println(d);
-        long l = o1.add(10,3.5);
-        System.out.println(l);
+        // long l = o1.add(10,3.5);
+        // System.out.println(l);
+        o1.add(22, 40);
 
     }
 }
