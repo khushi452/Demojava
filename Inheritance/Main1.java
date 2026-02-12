ca6f7e2c5d4ccaab46f310d7ef3a27ec984998bb
khushi452 <khushisharmaclass10@gmail.com>
2026-02-12 08:57:38 +0530

    parent child class

---
+++ Inheritance/Main1.java
@@ -0,0 +1,30 @@
+
+class Parent{
+    void dispaly(){
+        System.out.println("I am display() of Parent class");
+
+    }
+}
+)
+class Parent1{
+    void display(){
+        System.out.println("I am display() of Parent class");
+
+    }
+}
+class Child extends Parent1{
+    void show(){
+        System.out.println("I am show() of Child class");
+    }   
+}
+public class Main1 {
+    public static void main(String[] args) {
+        Child1 C = new Child1();
+        c.a = 10;
+        c.b = 20;
+        c.c = 30;
+
+        
+    }
+    
+}
