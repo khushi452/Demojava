e8affca6de343289c02210b48900c347f9991e49
khushi452 <khushisharmaclass10@gmail.com>
2026-03-20 12:51:19 +0530

    switch

---
+++ SWITCH/Main3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter the num:");
+        int r = S.nextInt();
+
+        int res = switch(r) {
+            case 1 :
+                yield 100;
+            case 2 :
+                yield 300;
+            default :
+                 yield 0;
+        };
+
+        System.out.println("Result: " + res);
+        S.close();
+    }
+}
