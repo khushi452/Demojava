7f37815c70783700f1700aabdcb520c7727b76db
khushi452 <khushisharmaclass10@gmail.com>
2026-03-24 09:28:35 +0530

    thread timed waiting

--- SWITCH/THREAD.java
+++ SWITCH/THREAD.java
@@ -1,35 +1,43 @@
-
-class Class1 extends Thread{
-    public void run(){
-        for(int i=1;i<=5;i++)
+class Class1 extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
             System.out.print(i);
-
+        }
     }
 }
 
-
-class Class2 implements Runnable{
-    public void run()
-    {
-        for(char i='A';i<='E';i++)
+class Class2 implements Runnable {
+    public void run() {
+        for (char i = 'A'; i <= 'E'; i++) {
             System.out.print(i);
+        }
     }
-    
 }
 
-public class  THREAD{
+public class THREAD {
     public static void main(String[] args) {
-        Class1 T1= new Class1();
-        Class2 C1=new Class2();
-        Thread T2= new Thread(C1,"Thread two");
-        //Priorities
-        // T1.setPriority(1);
-        // T2.setPriority(10);
+
+        Class1 T1 = new Class1();
+        Class2 C1 = new Class2();
+
+        Thread T2 = new Thread(C1, "Thread two");
+
+        // Setting priorities (optional)
         T1.setPriority(Thread.MAX_PRIORITY);
         T2.setPriority(Thread.MIN_PRIORITY);
+
+        // Start threads
         T1.start();
         T2.start();
 
+        try {
+            // Wait for both threads to finish
+            T1.join();
+            T2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted: " + e.getMessage());
+        }
+
+        System.out.println("\nMain thread finished");
     }
-    
 }
