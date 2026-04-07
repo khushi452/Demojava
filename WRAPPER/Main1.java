7a687d461c521cd212169b432fc830c47ce85c71
khushi452 <khushisharmaclass10@gmail.com>
2026-04-07 09:32:42 +0530

    wrapper

---
+++ WRAPPER/Main1.java
@@ -0,0 +1,9 @@
+public class Main1 {
+    public static void main(String[] args) {
+        Integer I1 = 10;
+        int a = I1.intValue();
+        System.out.println("Primitive value of a: " + a);
+        
+    }
+    
+}
