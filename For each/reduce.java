473c2b5cc1a57622942f1aade9b2467271621164
khushi452 <khushisharmaclass10@gmail.com>
2026-04-10 12:57:19 +0530

    reduce

--- For each/reduce.java
+++ For each/reduce.java
@@ -6,6 +6,7 @@ public class reduce {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
 
         int sum = list.stream()
+            .filter(n -> n % 2 != 0) // filter odd numbers
             .reduce(0, (a, b) -> a + b); // reduce to sum
 
         System.out.println("Sum: " + sum);
