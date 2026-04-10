9b3172333aef73a3216523ad5222af7e46b7d82d
khushi452 <khushisharmaclass10@gmail.com>
2026-04-10 09:05:17 +0530

    record

---
+++ For each/Main.java
@@ -0,0 +1,14 @@
+record Employee(int eid, String Name,double sat){
+
+    
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Employee E1 = new Employee(1,"Alice",100.52);
+        System.out.println(E1);
+
+        
+    }
+    
+}
