55d18ddcf1e1110e14e8d29b393567d93800502a
khushi452 <khushisharmaclass10@gmail.com>
2026-04-10 12:40:30 +0530

    filter

---
+++ For each/filter.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class filter {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(5, 3, 1, 4, 2);
+
+        List<Integer> res = list.stream()
+            .filter(n -> n % 2 == 0)   // filter even numbers
+            .collect(Collectors.toList());
+
+        System.out.println(res);
+    }
+}
