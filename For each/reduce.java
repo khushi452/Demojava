4664fd00b9e85ae26f47d08c8ba42cd3b0e5ee87
khushi452 <khushisharmaclass10@gmail.com>
2026-04-10 12:55:50 +0530

    reduce

---
+++ For each/reduce.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class reduce {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+        int sum = list.stream()
+            .reduce(0, (a, b) -> a + b); // reduce to sum
+
+        System.out.println("Sum: " + sum);
+    }
+    
+}
