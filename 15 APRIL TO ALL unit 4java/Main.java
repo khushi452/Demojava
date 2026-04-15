c5a0b532f75fbfa5d2ebbe2e093f526b3eb8af6c
khushi452 <khushisharmaclass10@gmail.com>
2026-04-15 11:10:54 +0530

    collection iterable

--- 15 APRIL TO ALL unit 4java/Main.java
+++ 15 APRIL TO ALL unit 4java/Main.java
@@ -1,10 +1,19 @@
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
         Iterable<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-        for(Integer i : list) {
-            System.out.println(i+ " ");
+        // //enhanced for each loop
+        // for(Integer i : list) {
+        //     System.out.println(i+ " ");
+        // }
+        // // //for each method
+        // list.forEach(n-> System.out.println(n + " "))
+        // iterator interface;
+        Iterator<Integer> itr = list.iterator();
+        while(itr.hasNext()) {
+            System.out.println(itr.next() + " ");
         }
     }
 }
