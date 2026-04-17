b4364817a21d08ac776066a5fd70ec44f6e8f4f2
khushi452 <khushisharmaclass10@gmail.com>
2026-04-17 09:01:22 +0530

    Collection List

---
+++ 15 APRIL TO ALL unit 4java/Main5.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class Main5{
+    public static void main(String[] args) {
+        List<Integer> list=new ArrayList<>();
+        list.add(10);
+        list.add(null);
+        System.out.println(list);
+    }
+    
+}
---
+++ 15 APRIL TO ALL unit 4java/Main6.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main6 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        Integer a[]= {10,20,30,40,50};
+        for (Integer i : a) {
+            list.add(i);
+        }   
+        System.out.println("List: " + list);
+
+    }
+
+
+    
+
+
+}
