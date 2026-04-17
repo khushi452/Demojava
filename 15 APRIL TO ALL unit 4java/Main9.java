91793a22aa6540ec08e3a0c821d3b62ec8ba2b9b
khushi452 <khushisharmaclass10@gmail.com>
2026-04-17 09:33:19 +0530

    Collection list sorting

---
+++ 15 APRIL TO ALL unit 4java/Main9.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.*;
+public class Main9
+ {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();//wrapper class integer we can use linkedlist arraylist 
+        System.out.println("List: " + list);    
+        list.add(10);
+        list.add(20);           
+        list.add(15);
+        System.out.println("List: " + list);
+        list.set(2, 1000);
+        System.out.println("List: " + list);
+         
+    }
+    
+}
