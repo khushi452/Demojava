62fe7a8e7e657a9498de52e6847bea254e62c760
khushi452 <khushisharmaclass10@gmail.com>
2026-04-17 09:41:19 +0530

    Collection addall

---
+++ 15 APRIL TO ALL unit 4java/Main10.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.*;
+public class Main10
+ {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();//wrapper class integer we can use linkedlist arraylist 
+        System.out.println("List: " + list1);    
+        list1.add(10);
+        list1.add(20);           
+        list1.add(15);
+        System.out.println("List: " + list1);
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(100);
+        list2.add(200); 
+        list1.addAll(list2);
+        System.out.println("List after adding all elements from list2: " + list1);
+    }
+    
+}
