61476067bad75e968608e112400175faf7daa145
khushi452 <khushisharmaclass10@gmail.com>
2026-04-17 09:45:06 +0530

    Collection clasess

--- 15 APRIL TO ALL unit 4java/Main10.java
+++ 15 APRIL TO ALL unit 4java/Main10.java
@@ -16,6 +16,10 @@ public class Main10
         list2.add(200); 
         list1.addAll(list2);
         System.out.println("List after adding all elements from list2: " + list1);
+        list1.addAll(0,list2);
+        System.out.println("List after adding all elements from list2 at index 0: " + list1);
+        System.out.println("Element at index 2: " + list1.get(2));
+        System.out.println("Is the list empty? " + list1.isEmpty());
     }
     
 }
